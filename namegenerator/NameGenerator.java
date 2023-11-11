0c1c56d510956da8e9415384dfe9e776880c7eb5
Pharrell Kaim <79710229+PharrellKaim@users.noreply.github.com>
2023-11-11 19:24:15 +0100

    Adding User requests
    This patch includes user querries to name the file

--- namegenerator/NameGenerator.java
+++ namegenerator/NameGenerator.java
@@ -1,20 +1,32 @@
+import java.util.Scanner;
+
 public class NameGenerator{
-    private void generation(String Category, int Date, String Priority, String FileName) {
+    private void generation( String Date, String Priority, String Category, String FileName) {
         System.out.println("Some Suggestions:");
         System.out.println(Date+"-"+Priority+"-"+Category+"-"+FileName);
 
-        //tbd: more suggestions
         //tbd: create better naming, maybe with translation etc
     }
 
     public static void main(String[] args){
+        //Generate instance and Scanner
         NameGenerator NameGenerator = new NameGenerator();
-        String Category = "W";
-        int Date = 102022;
-        String Priority = "C";
-        String FileName = "TestFile";
-        NameGenerator.generation(Category, Date, Priority, FileName);
-        //input has to be done 
-        // more testing -> very fast project, bc no time :) 
+        Scanner in = new Scanner(System.in);
+
+        //Request date
+        System.out.println("Enter your Date (ddmmyyy or mmyyyy or yyyy): ");
+        String date = in.nextLine();
+        //Request Priority
+        System.out.println("Enter your Priority (A,B,C): ");
+        String priority = in.nextLine();
+        //Request Category
+        System.out.println("Enter Your Category: ");
+        String category = in.nextLine();
+        //Request Filename
+        System.out.println("Name your File: ");
+        String fileName = in.nextLine();
+
+        NameGenerator.generation( date, priority, category, fileName);
+        //maybe: genenrate some more suggestions
     }
 }
